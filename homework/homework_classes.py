3e31108a6ffb3d4e4dea4e72e96b94ec28ba8f31
mr_eugene <roberttimethone@gmail.com>
2026-02-03 22:43:42 +0100

    sol

--- homework/homework_classes.py
+++ homework/homework_classes.py
@@ -19,3 +19,63 @@ FileServer(Server) — додає приватний атрибут __storage_us
 4. У кожному класі-спадкоємці:
 Перевизнач метод info(), щоб він відображав також і специфічні метрики конкретного типу сервера.
 """
+
+class Server:
+
+    def __status_changer(self, new_status: bool) -> str:
+        if new_status:
+            return "running"
+        elif not new_status:
+            return "stopped"
+        else:
+            print("Status must be eather True or False, now status automatically set to False")
+            return "stopped"
+
+    def __init__(self, name: str, ip: int, status: bool):
+        self._name = name
+        self._ip = ip
+        self._status = self.__status_changer(status)
+
+    def info(self):
+        return f"Server {self._name}, ip: {self._ip}, status:{self._status}"
+
+    def ping(self):
+        return f"Server {self._name} is {self._status}"
+
+
+class WebServer(Server):
+    def __init__(self, name, ip, status, requests_per_minute):
+        super().__init__(name, ip, status)
+        self.__requests_per_minute = requests_per_minute
+
+    def info(self):
+        return f"Server {self._name}, ip: {self._ip}, status:{self._status}, requests per minute: {self.__requests_per_minute}"
+
+
+class DatabaseServer(Server):
+    def __init__(self, name, ip, status, active_connections):
+        super().__init__(name, ip, status)
+        self.__active_connections = active_connections
+
+    def info(self):
+        return f"Server {self._name}, ip: {self._ip}, status:{self._status}, active connections: {self.__active_connections}"
+
+
+class FileServer(Server):
+    def __init__(self, name, ip, status, storage_usage):
+        super().__init__(name, ip, status)
+        self.__storage_usage = storage_usage
+
+    def info(self):
+        return f"Server {self._name}, ip: {self._ip}, status:{self._status}, storage usage: {self.__storage_usage}"
+
+
+ser = Server("digger", "1.2.3.4", True)
+print(ser.info())
+print(ser.ping())
+webser = WebServer("nigger", "1.4.8.8", True, 10)
+print(webser.info())
+print(webser.ping())
+fileser = FileServer("singer", "6.6.6", False, 1000)
+print(fileser.info())
+print(fileser.ping())

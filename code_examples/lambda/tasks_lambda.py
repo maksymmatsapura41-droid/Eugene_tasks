fd6b35b108333102ca5008181f6779a31a6557a1
mr_eugene <roberttimethone@gmail.com>
2026-02-09 21:23:07 +0100

    sol lambda

--- code_examples/lambda/tasks_lambda.py
+++ code_examples/lambda/tasks_lambda.py
@@ -1,6 +1,6 @@
 # Find negative numbers and replace them with zero
 nums = [3, -1, 0, 7, -5, -10, 12]
-
+print(list(map(lambda x: x if x >= 0 else 0, nums)))
 
 # Found users older than 18 and name starts with "O"
 users = [
@@ -9,7 +9,7 @@ users = [
     {"name": "Ivan", "age": 30},
     {"name": "Olga", "age": 19}
 ]
-
+print(list(filter(lambda x: x["age"] > 18 and x["name"].startswith("O"), users)))
 
 # Sort users by age in reverse order
 users_1 = [
@@ -17,7 +17,8 @@ users_1 = [
     {"name": "Oleg", "age": 17},
     {"name": "Ivan", "age": 30}
 ]
-
+users_1.sort(key=lambda x: x["age"], reverse=True)
+print(users_1)
 
 #Create list of products where price that is below 10 stay the same it and if bigger than 10 increase price by 50 percents
 products = [
@@ -25,3 +26,5 @@ products = [
     {"name": "banana", "price": 5},
     {"name": "cherry", "price": 20}
 ]
+
+print(list(map(lambda x: x if x["price"] < 10 else {**x, "price": x["price"]*1.5}, products)))
